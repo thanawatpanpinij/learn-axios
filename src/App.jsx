df287a0e2ece00ec7c163f122d31b7bf85b0bc96
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-03-31 17:11:20 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div className="h-screen bg-blue-950">
-      <Navbar />
-      <div className="flex justify-center bg-blue-950">
-        <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-          <h1 className="w-full p-6 bg-amber-100 font-extrabold text-center text-4xl">
-            My React Template
-          </h1>
-          <section className="w-full p-5 bg-amber-100 flex">
-            <ul className="list-inside list-disc flex-1">
-              <span className="font-semibold">Tech Stack:</span>
-              <li>Vite</li>
-              <li>React</li>
-              <li>JavaScript</li>
-              <li>Tailwind</li>
-            </ul>
-          </section>
+    const [inputValue, setInputValue] = useState("");
+    const [meal, setMeal] = useState(null);
+    async function fetchMeal() {
+        try {
+            const data = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`);
+            console.log(data);
+            console.log(data.data.meals[0].strMeal);
+            setMeal(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <div className="grid place-items-center min-h-screen bg-blue-950">
+            <div className="w-[80%]">
+                <input className="block mx-auto mb-4 px-4 py-2 bg-white rounded" type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue} placeholder="Enter some meals" />
+                <button className="block m-auto mb-8 px-4 py-2 bg-gray-200 rounded-md hover:cursor-pointer" onClick={fetchMeal}>
+                    Search
+                </button>
+                {meal && <p className="text-center text-white">Your meal is {meal.data.meals[0].strMeal}</p>}
+            </div>
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 }
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="absolute bottom-0 w-full text-center text-amber-50 p-4 bg-black">
-      Footer
-    </div>
-  );
-}
